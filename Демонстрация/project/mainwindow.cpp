5ede631a04ff601a910b2d614dae36875fe46786
Paveloom <paveloom@mail.ru>
2019-08-09 19:45:42 +0300

    Все параметры: Добавлены все параметры из файлов settings в репозитории C1. Растягивание окна теперь запрещено.

--- Демонстрация/project/mainwindow.cpp
+++ Демонстрация/project/mainwindow.cpp
@@ -10,13 +10,35 @@ MainWindow::MainWindow(QWidget *parent) :
 
     QSettings settings(ORGANIZATION_NAME, APPLICATION_NAME);
 
-    ui->checkBox->setChecked(settings.value(SETTINGS_CHECKBOX_1, false).toBool());
-    ui->checkBox_2->setChecked(settings.value(SETTINGS_CHECKBOX_2, false).toBool());
-
-    ui->checkBox_2->setEnabled(settings.value(SETTINGS_CHECKBOX_2_ENABLED, ui->checkBox->isChecked()).toBool());
-
     ui->lineEdit_1->setText(settings.value(SETTINGS_LINEEDIT_1, "").toString());
     ui->lineEdit_1->setValidator( new QIntValidator(this) );
+
+    ui->checkBox->setChecked(settings.value(SETTINGS_CHECKBOX_1, false).toBool());
+
+    ui->checkBox_2->setChecked(settings.value(SETTINGS_CHECKBOX_2, false).toBool());
+    ui->checkBox_2->setEnabled(settings.value(SETTINGS_CHECKBOX_2_ENABLED, ui->checkBox->isChecked()).toBool());
+
+    ui->lineEdit_2->setText(settings.value(SETTINGS_LINEEDIT_2, "").toString());
+    ui->lineEdit_2->setValidator( new QIntValidator(this) );
+    ui->lineEdit_2->setEnabled(settings.value(SETTINGS_LINEEDIT_2_ENABLED, ui->checkBox_2->isChecked() and ui->checkBox->isChecked()).toBool());
+
+    ui->checkBox_3->setChecked(settings.value(SETTINGS_CHECKBOX_3, false).toBool());
+
+    ui->checkBox_4->setChecked(settings.value(SETTINGS_CHECKBOX_4, false).toBool());
+
+    ui->lineEdit_3->setText(settings.value(SETTINGS_LINEEDIT_3, "").toString());
+    ui->lineEdit_3->setValidator( new QIntValidator(this) );
+
+    ui->lineEdit_4->setText(settings.value(SETTINGS_LINEEDIT_4, "").toString());
+    ui->lineEdit_4->setValidator( new QIntValidator(this) );
+
+    ui->lineEdit_5->setText(settings.value(SETTINGS_LINEEDIT_5, "").toString());
+    ui->lineEdit_5->setValidator( new QIntValidator(this) );
+    ui->lineEdit_5->setEnabled(settings.value(SETTINGS_LINEEDIT_5_ENABLED, not ui->checkBox_3->isChecked()).toBool());
+
+    ui->lineEdit_6->setText(settings.value(SETTINGS_LINEEDIT_6, "").toString());
+    ui->lineEdit_6->setValidator( new QIntValidator(this) );
+    ui->lineEdit_6->setEnabled(settings.value(SETTINGS_LINEEDIT_6_ENABLED, not ui->checkBox_3->isChecked()).toBool());
 
 }
 
@@ -29,19 +51,28 @@ void MainWindow::on_saveButton_clicked()
 {
     QSettings settings(ORGANIZATION_NAME, APPLICATION_NAME);
 
-    if(ui->checkBox->isChecked()){
-        settings.setValue(SETTINGS_CHECKBOX_1, true);
-    } else {
-        settings.setValue(SETTINGS_CHECKBOX_1, false);
-    }
-
-    if(ui->checkBox_2->isChecked()){
-        settings.setValue(SETTINGS_CHECKBOX_2, true);
-    } else {
-        settings.setValue(SETTINGS_CHECKBOX_2, false);
-    }
-
     settings.setValue(SETTINGS_LINEEDIT_1, ui->lineEdit_1->text());
+
+    settings.setValue(SETTINGS_CHECKBOX_1, ui->checkBox->isChecked());
+
+    settings.setValue(SETTINGS_CHECKBOX_2, ui->checkBox_2->isChecked());
+    settings.setValue(SETTINGS_CHECKBOX_2_ENABLED, ui->checkBox_2->isEnabled());
+
+    settings.setValue(SETTINGS_LINEEDIT_2, ui->lineEdit_2->text());
+    settings.setValue(SETTINGS_LINEEDIT_2_ENABLED, ui->lineEdit_2->isEnabled());
+
+    settings.setValue(SETTINGS_CHECKBOX_3, ui->checkBox_3->isChecked());
+
+    settings.setValue(SETTINGS_CHECKBOX_4, ui->checkBox_4->isChecked());
+
+    settings.setValue(SETTINGS_LINEEDIT_3, ui->lineEdit_3->text());
+
+    settings.setValue(SETTINGS_LINEEDIT_4, ui->lineEdit_4->text());
+
+    settings.setValue(SETTINGS_LINEEDIT_5, ui->lineEdit_5->text());
+    settings.setValue(SETTINGS_LINEEDIT_6, ui->lineEdit_6->text());
+    settings.setValue(SETTINGS_LINEEDIT_5_ENABLED, ui->lineEdit_5->isEnabled());
+    settings.setValue(SETTINGS_LINEEDIT_6_ENABLED, ui->lineEdit_6->isEnabled());
 
     settings.sync();
 
@@ -62,9 +93,36 @@ void MainWindow::on_checkBox_stateChanged()
 
     if(ui->checkBox->isChecked()){
         ui->checkBox_2->setEnabled(true);
-        settings.setValue(SETTINGS_CHECKBOX_2_ENABLED, true);
+
+        if(ui->checkBox_2->isChecked()){
+            ui->lineEdit_2->setEnabled(true);
+        }
     } else {
         ui->checkBox_2->setEnabled(false);
-        settings.setValue(SETTINGS_CHECKBOX_2_ENABLED, false);
+
+        ui->lineEdit_2->setEnabled(false);
+    }
+
+}
+
+void MainWindow::on_checkBox_2_stateChanged()
+{
+
+    if(ui->checkBox->isChecked() and ui->checkBox_2->isChecked()){
+        ui->lineEdit_2->setEnabled(true);
+    } else {
+        ui->lineEdit_2->setEnabled(false);
+    }
+
+}
+
+void MainWindow::on_checkBox_3_stateChanged()
+{
+    if(ui->checkBox_3->isChecked()){
+        ui->lineEdit_5->setEnabled(false);
+        ui->lineEdit_6->setEnabled(false);
+    } else {
+        ui->lineEdit_5->setEnabled(true);
+        ui->lineEdit_6->setEnabled(true);
     }
 }
--- Демонстрация/project/mainwindow.h
+++ Демонстрация/project/mainwindow.h
@@ -16,9 +16,27 @@
 #define APPLICATION_NAME "Demonstration"
 
 #define SETTINGS_LINEEDIT_1 "setting/lineedit_1"
+
 #define SETTINGS_CHECKBOX_1 "settings/checkbox_1"
+
 #define SETTINGS_CHECKBOX_2 "settings/checkbox_2"
 #define SETTINGS_CHECKBOX_2_ENABLED "settings/checkbox_2_enabled"
+
+#define SETTINGS_LINEEDIT_2 "setting/lineedit_2"
+#define SETTINGS_LINEEDIT_2_ENABLED "setting/lineedit_2_enabled"
+
+#define SETTINGS_CHECKBOX_3 "setting/checkbox_3"
+
+#define SETTINGS_CHECKBOX_4 "setting/checkbox_4"
+
+#define SETTINGS_LINEEDIT_3 "setting/lineedit_3"
+
+#define SETTINGS_LINEEDIT_4 "setting/lineedit_4"
+
+#define SETTINGS_LINEEDIT_5 "setting/lineedit_5"
+#define SETTINGS_LINEEDIT_5_ENABLED "setting/lineedit_5_enabled"
+#define SETTINGS_LINEEDIT_6 "setting/lineedit_6"
+#define SETTINGS_LINEEDIT_6_ENABLED "setting/lineedit_6_enabled"
 
 #define FILENAME "settings"
 
@@ -37,6 +55,8 @@ public:
 private slots:
     void on_saveButton_clicked();
     void on_checkBox_stateChanged();
+    void on_checkBox_2_stateChanged();
+    void on_checkBox_3_stateChanged();
 
 private:
     Ui::MainWindow * ui;
